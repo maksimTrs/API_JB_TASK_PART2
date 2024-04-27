8702fbd82534ea28327892d9712ad39401c5b406
maksimTrs <roronoazorroippo@gmail.com>
2024-04-27 18:50:13 +0400

    update JbChangeLicensesTeamPositiveTest postitive TCs

--- src/test/java/tests/changeLicensesTeamApi/JbChangeLicensesTeamPositiveTest.java
+++ src/test/java/tests/changeLicensesTeamApi/JbChangeLicensesTeamPositiveTest.java
@@ -1,7 +1,6 @@
 package tests.changeLicensesTeamApi;
 
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeMethod;
@@ -9,13 +8,12 @@ import org.testng.annotations.Test;
 import pojo.changeLicensesTeam.ChangeLicensesTeamObject;
 import tests.BaseTest;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static api.ApiConstants.*;
+import static api.ApiConstants.CHANGE_LICENSE;
+import static api.ApiConstants.TEAM001_ID_CODE;
 import static api.ResponseHelper.extractApiResponse;
 import static enums.StatusCodeEnum.CODE_200;
-import static org.assertj.core.api.Assertions.assertThat;
 import static steps.TestSteps.prepareLicensesForTheTeam;
 import static utils.AssertionHelper.assertLicenseIdsResponseBody;
 import static utils.PropertyReader.getLicenseListFromBundle;
@@ -26,17 +24,17 @@ public class JbChangeLicensesTeamPositiveTest extends BaseTest {
     private static final String FIRST_LICENSE = LICENSE_LIST.get(0);
     private final List<String> LICENSE_EMPTY_LIST = List.of();
 
+
     @BeforeMethod
     private void beforeTestMethod() {
         prepareLicensesForTheTeam(LICENSE_LIST);
     }
 
+
     @Story("Testing POST api/v1/customer/changeLicensesTeam")
     @Description("Testing API: Payload with single license id")
     @Test()
     public void verifyChangeSingleLicenseTest() {
-      //  prepareLicensesForTheTeam(LICENSE_LIST);
-
         ChangeLicensesTeamObject changeLicensesTeamObject = ChangeLicensesTeamObject.builder()
                 .targetTeamId(TEAM001_ID_CODE)
                 .licenseIds(List.of(FIRST_LICENSE))
@@ -50,8 +48,6 @@ public class JbChangeLicensesTeamPositiveTest extends BaseTest {
     @Description("Testing API: Payload with multiple license ids")
     @Test()
     public void verifyChangeMultipleLicensesTest() {
-     //   prepareLicensesForTheTeam(LICENSE_LIST);
-
         ChangeLicensesTeamObject changeLicensesTeamObject = ChangeLicensesTeamObject.builder()
                 .targetTeamId(TEAM001_ID_CODE)
                 .licenseIds(LICENSE_LIST)
@@ -66,8 +62,6 @@ public class JbChangeLicensesTeamPositiveTest extends BaseTest {
     @Description("Testing API: Payload with empty list of the license ids")
     @Test()
     public void verifyChangeEmptyLicensesTest() {
-      //  prepareLicensesForTheTeam(LICENSE_LIST);
-
         ChangeLicensesTeamObject changeLicensesTeamObject = ChangeLicensesTeamObject.builder()
                 .targetTeamId(TEAM001_ID_CODE)
                 .licenseIds(LICENSE_EMPTY_LIST)
@@ -76,5 +70,4 @@ public class JbChangeLicensesTeamPositiveTest extends BaseTest {
         Response response = extractApiResponse(changeLicensesTeamObject, CHANGE_LICENSE);
         assertLicenseIdsResponseBody(response, CODE_200.CODE, LICENSE_EMPTY_LIST);
     }
-
 }
